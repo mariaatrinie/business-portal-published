b8acb12e4136a57e5e5ef629002d8830252df49a
mariaatrinie <mariaatrinie@gmail.com>
2026-01-31 16:21:42 +0530

    Fix Render add business issue

--- backend/server.js
+++ backend/server.js
@@ -6,24 +6,21 @@ const path = require("path");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // 🔴 REQUIRED
 
-// MongoDB LOCAL connection (Compass)
-mongoose.connect("mongodb://127.0.0.1:27017/businessPortal")
-  .then(() => console.log("MongoDB Local Connected"))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error(err));
 
-// API routes
 app.use("/api/business", require("./routes/businessRoutes"));
 
-// Serve frontend
 app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
-// Start server
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running");
 });
+
